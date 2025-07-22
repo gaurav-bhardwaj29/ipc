5053a47c8994542dd5c063d90f1a571f7274e393
gaurav-bhardwaj29 <gaurav290802@gmail.com>
2025-07-23 01:57:04 +0530

    semaphore rm

---
+++ semrm.c
@@ -0,0 +1,28 @@
+// removes a semaphore
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+int main(void)
+{
+    key_t key;
+    int semid;
+    if ((key = ftok("semdemo.c", 'J')) == -1){
+        perror("ftok");
+        exit(1);
+    }
+    if ((semid = semget(key, 1, 0)) == -1){
+        perror("semget");
+        exit(1);
+    }
+    if (semctl(semid, 0, IPC_RMID) == -1)
+    {
+        perror("semctl");
+        exit(1);
+    }
+    return 0;
+}
