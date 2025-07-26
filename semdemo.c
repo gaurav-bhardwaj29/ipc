e164a7122f0fda334688d4530b4bedaae778f8a5
gaurav-bhardwaj29 <gaurav290802@gmail.com>
2025-07-27 00:51:12 +0530

    semaphore

--- semdemo.c
+++ semdemo.c
@@ -6,34 +6,20 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
-
 #define MAX_RETRIES 10
 #define NEED_SEMUM
-
-// #ifdef NEED_SEMUM
-// union semun {
-//     int val;
-//     struct semid_ds *buf;
-//     ushort *array;
-// };
-// #endif
-
-int initsem(key_t key, int nsems)
-{
+int initsem(key_t key, int nsems){
     int i;
     union semun arg;
     struct semid_ds buf;
     struct sembuf sb = { 0 };
     int semid;
-
     semid = semget (key, nsems, IPC_CREAT | IPC_EXCL | 0666);
-    if (semid >=0 )
-    {
+    if (semid >=0 ){
        sb.sem_op = 1; sb.sem_flg = 0;
        arg.val = 1;
        printf("press return.\n"); getchar ();
-       for (sb.sem_num=0; sb.sem_num<nsems; sb.sem_num++)
-       {
+       for (sb.sem_num=0; sb.sem_num<nsems; sb.sem_num++){
             /*do a semop() to "free" the semaphores. */
             /*this sets the sem_otime field, as needed below. */
             if (semop(semid, &sb, 1) == -1){
@@ -51,28 +37,26 @@ int initsem(key_t key, int nsems)
         // wait for other processes to initialize the semaphore:
 
         arg.buf = &buf;
-        for (i=0; i<MAX_RETRIES && !ready; i++)
-        {
+        for (i=0; i<MAX_RETRIES && !ready; i++){
             semctl(semid, nsems-1, IPC_STAT, arg);
-            if (arg.buf -> sem_otime != 0)
-            {
+            if (arg.buf -> sem_otime != 0){
                 ready = 1;
-            }else {
-                sleep(1);
             }
+            else {
+                sleep(1);  }
         }
         if(!ready){
             errno = ETIME;
             return -1;
         }
-       }
+  }
        else {
         return semid; // error, check errno
        }
 return semid;
     }
     
-
+    
     int main(void){
         key_t key;
         int semid;
